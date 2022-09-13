32520a34c78aaaaaf005c005a9e85564d1bd44bd
minji <minjikim@goldenplanet.co.kr>
2022-09-13 16:37:33 +0900

    로그인 페이지 추가

--- src/components/Pages/LoginPage/LoginPage.js
+++ src/components/Pages/LoginPage/LoginPage.js
@@ -1,7 +1,74 @@
 import React from "react";
+import { Button, Form, Input } from "antd";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  let navigate = useNavigate();
+
+  const onFinish = (values) => {
+    console.log("Success:", values);
+    navigate("/");
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <div className="LoginPage">
+      <div className="LoginPage_container">
+        <Form
+          name="basic"
+          // labelCol={{
+          //   span: 8,
+          // }}
+          // wrapperCol={{
+          //   span: 16,
+          // }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: "Please input your username!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item
+            wrapperCol={{
+              offset: 10,
+              span: 16,
+            }}
+          >
+            <Button type="primary" htmlType="submit">
+              로그인
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
